9aa1ef0db536fbc18a128fab0050d65fac9604dd
Lhcfl <lhcfl@outlook.com>
2025-10-11 17:20:16 +0800

    ♻️ refactor of mk note header

--- src/components/note/mk-note-header.tsx
+++ src/components/note/mk-note-header.tsx
@@ -4,24 +4,43 @@ import { MkAvatar } from '../mk-avatar'
 import { MkTime } from '../mk-time'
 import { MkUserName } from '../mk-user-name'
 import { MkVisibilityIcon } from './mk-visibility-icon'
+import { Avatar, AvatarImage } from '../ui/avatar'
 
 export const MkNoteHeader = (props: { note: NoteWithExtension }) => {
+  const { note } = props
+  const metaName = useSiteMeta(s => s.name)
+  const metaIcon = useSiteMeta(s => s.iconUrl)
+  const instanceName = note.user.instance?.name || metaName || undefined
+  const instanceIcon = note.user.instance?.iconUrl || metaIcon || undefined
+
   return (
     <div className="mk-note-header flex items-center gap-2 p-2">
-      <MkAvatar user={props.note.user} className="size-10 z-10" />
-      <div className="user-info flex-grow-1 flex-shrink-1 w-0">
-        <div className="user-name font-bold">
-          <MkUserName user={props.note.user} />
+      <MkAvatar user={note.user} className="size-10 z-10" />
+      <div className="flex-grow-1 flex-shrink-1 w-0 flex flex-col">
+        <div className="flex justify-between gap-1">
+          <div className="user-name font-bold flex-grow-1 flex-shrink-1 w-0 line-clamp-1">
+            <MkUserName user={note.user} />
+          </div>
+          <div className="note-time flex items-center gap-2 flex-shrink-0">
+            <MkTime time={note.createdAt} className="text-sm" />
+            <MkVisibilityIcon className="size-4" note={note} />
+          </div>
         </div>
-        <div className="user-username text-sm text-gray-500">
-          @
-          {acct.toString(props.note.user)}
-        </div>
-      </div>
-      <div className="note-info">
-        <div className="note-time flex items-center gap-2">
-          <MkTime time={props.note.createdAt} className="text-sm" />
-          <MkVisibilityIcon className="size-4" note={props.note} />
+        <div className="flex justify-between gap-2">
+          <div className="user-username text-sm text-gray-500 flex-shrink-0">
+            @
+            {acct.toString(note.user)}
+          </div>
+          <div className="flex-grow-1 flex-shrink-1 w-0">
+            <div className="note-instance ml-auto max-w-full text-xs flex items-center gap-1 px-1 py-0.5 bg-muted rounded-xl w-fit">
+              <Avatar className="size-3">
+                <AvatarImage src={instanceIcon} />
+              </Avatar>
+              <span className="truncate">
+                {instanceName}
+              </span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
