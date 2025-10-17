feb1e70da6ca8066a003ca0e0f13520352c6ac46
Lhcfl <lhcfl@outlook.com>
2025-10-17 16:07:21 +0800

    🐛 fix: use menuordrawer in renote tip

--- src/components/note/mk-note-renote-tip.tsx
+++ src/components/note/mk-note-renote-tip.tsx
@@ -4,21 +4,30 @@ import { MkAvatar } from '@/components/mk-avatar';
 import { MkTime } from '@/components/mk-time';
 import { MkUserName } from '@/components/mk-user-name';
 import { Button } from '@/components/ui/button';
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu';
 import { MkVisibilityIcon } from './mk-visibility-icon';
 import type { NoteWithExtension } from '@/types/note';
 import { useMe } from '@/stores/me';
 import { useDeleteNoteAction } from '@/hooks/note-actions';
+import { MenuOrDrawer, type Menu } from '../menu-or-drawer';
 
 export const MkNoteRenoteTip = (props: { note: NoteWithExtension }) => {
   const meId = useMe((me) => me.id);
   const { note } = props;
   const isMine = note.userId === meId;
+  const { mutate: unrenote } = useDeleteNoteAction(note.id);
+  const { t } = useTranslation();
+  const menu: Menu = [
+    {
+      type: 'item',
+      id: 'unrenote',
+      icon: <Trash2Icon />,
+      label: t('unrenote'),
+      variant: 'destructive',
+      onClick: () => {
+        unrenote();
+      },
+    },
+  ];
 
   return (
     <div className="mk-note-renote-tip p-2 flex gap-2 items-center text-muted-foreground">
@@ -26,39 +35,16 @@ export const MkNoteRenoteTip = (props: { note: NoteWithExtension }) => {
       <MkAvatar user={note.user} avatarProps={{ className: 'size-6' }} />
       <MkUserName user={note.user} className="flex-grow-1 flex-shrink-1 w-0 line-clamp-1" />
       <div className="renote-info flex gap-2 items-center">
-        {isMine && <RenoteMore note={note} />}
+        {isMine && (
+          <MenuOrDrawer menu={menu}>
+            <Button size="icon" variant="ghost">
+              <MoreVerticalIcon className="size-4" />
+            </Button>
+          </MenuOrDrawer>
+        )}
         <MkTime time={note.createdAt} className="text-sm" />
         <MkVisibilityIcon iconProps={{ className: 'size-4' }} note={note} />
       </div>
     </div>
-  );
-};
-
-const RenoteMore = (props: { note: NoteWithExtension }) => {
-  const { note } = props;
-  const { t } = useTranslation();
-
-  const { mutate: unrenote } = useDeleteNoteAction(note.id);
-
-  return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button size="icon" variant="ghost">
-          <MoreVerticalIcon className="size-4" />
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="start">
-        <DropdownMenuItem
-          className="cursor-pointer"
-          variant="destructive"
-          onClick={() => {
-            unrenote();
-          }}
-        >
-          <Trash2Icon />
-          {t('unrenote')}
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
   );
 };
