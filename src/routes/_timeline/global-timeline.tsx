dc510d9ebb9d12aa29a6fea389afa1755730bb8f
Lhcfl <lhcfl@outlook.com>
2025-10-19 23:58:00 +0800

    🚨 chore: make REUSE lint happy

--- src/routes/_timeline/global-timeline.tsx
+++ src/routes/_timeline/global-timeline.tsx
@@ -1,3 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: Linca and papilio-project
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
 import { MkTimeline } from '@/components/mk-timeline';
 import { createFileRoute } from '@tanstack/react-router';
 
