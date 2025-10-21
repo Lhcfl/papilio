cf6b9f67056b0ed8c584012279117f3752992629
Lhcfl <lhcfl@outlook.com>
2025-10-21 18:07:43 +0800

    ✨ feat: allow upload from file

--- src/components/mk-file-upload-menu.tsx
+++ src/components/mk-file-upload-menu.tsx
@@ -3,14 +3,19 @@
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
-import { HardDriveUploadIcon, UploadCloudIcon, UploadIcon } from 'lucide-react';
+import { HardDriveUploadIcon, LinkIcon, UploadCloudIcon, UploadIcon, XIcon } from 'lucide-react';
 import { MenuOrDrawer, type Menu } from '@/components/menu-or-drawer';
-import { toast } from 'sonner';
 import { useTranslation } from 'react-i18next';
 import { useUploader, type UploadFileOptions } from '@/hooks/use-uploader';
 import type { DriveFile } from 'misskey-js/entities.js';
 import { MkDriveFileSelect } from '@/components/mk-drive-file-select';
 import { useState } from 'react';
+import { useMutation } from '@tanstack/react-query';
+import { injectMisskeyStream, misskeyApi } from '@/services/inject-misskey-api';
+import { Dialog, DialogClose, DialogContent, DialogFooter, DialogTitle } from '@/components/ui/dialog';
+import { InputGroup, InputGroupAddon, InputGroupInput } from '@/components/ui/input-group';
+import { Button } from '@/components/ui/button';
+import { Spinner } from '@/components/ui/spinner';
 
 export const MkFileUploadMenu = (props: {
   children: React.ReactNode;
@@ -20,6 +25,8 @@ export const MkFileUploadMenu = (props: {
   const { children, limit = Number.POSITIVE_INFINITY, onUpload } = props;
   const { t } = useTranslation();
   const [openDriveSelect, setOpenDriveSelect] = useState(false);
+  const [openUrlUpload, setOpenUrlUpload] = useState(false);
+  const [isUploading, setIsUploading] = useState(false);
 
   const uploadFile = useUploader();
 
@@ -34,6 +41,32 @@ export const MkFileUploadMenu = (props: {
     };
     input.click();
   }
+
+  const { mutateAsync: uploadFromUrl } = useMutation({
+    mutationKey: ['upload-from-url'],
+    mutationFn: async ({ url, folderId }: { url: string; folderId?: string }) => {
+      const id = crypto.randomUUID();
+      const stream = injectMisskeyStream();
+      const channel = stream.useChannel('main');
+      const ret = new Promise<DriveFile>((resolve, reject) => {
+        setTimeout(() => {
+          reject(new Error(t('_reversi.timeout')));
+        }, 60 * 1000);
+        channel.on('urlUploadFinished', ({ marker, file }) => {
+          if (marker === id) {
+            resolve(file);
+            channel.dispose();
+          }
+        });
+      });
+      setIsUploading(true);
+      await misskeyApi('drive/files/upload-from-url', { url, folderId, marker: id }).finally(() => {
+        setIsUploading(false);
+        setOpenUrlUpload(false);
+      });
+      return ret;
+    },
+  });
 
   const menu: Menu = [
     { type: 'label', id: 'upload-label', label: t('attachFile') },
@@ -60,7 +93,9 @@ export const MkFileUploadMenu = (props: {
       id: 'upload-from-url',
       icon: <UploadCloudIcon />,
       label: t('uploadFromUrl'),
-      onClick: () => toast.error('Not implemented yet'),
+      onClick: () => {
+        setOpenUrlUpload(true);
+      },
     },
     {
       type: 'item',
@@ -85,6 +120,39 @@ export const MkFileUploadMenu = (props: {
           onUpload(files.map((f) => Promise.resolve(f)));
         }}
       />
+      <Dialog open={openUrlUpload} onOpenChange={setOpenUrlUpload}>
+        <DialogContent>
+          <DialogTitle>{t('uploadFromUrl')}</DialogTitle>
+          <form
+            onSubmit={(ev) => {
+              ev.preventDefault();
+              ev.stopPropagation();
+              const formData = new FormData(ev.currentTarget);
+              const url = formData.get('url') as string | null;
+              if (!url) return;
+              onUpload([uploadFromUrl({ url })]);
+            }}
+          >
+            <InputGroup>
+              <InputGroupAddon>
+                <LinkIcon />
+              </InputGroupAddon>
+              <InputGroupInput type="url" name="url" placeholder={t('imageUrl')} />
+            </InputGroup>
+            <DialogFooter className="mt-2">
+              <DialogClose asChild>
+                <Button variant="outline" type="reset">
+                  <XIcon /> {t('cancel')}
+                </Button>
+              </DialogClose>
+              <Button type="submit">
+                {isUploading ? <Spinner /> : <UploadCloudIcon />}
+                {t('uploadFromUrl')}
+              </Button>
+            </DialogFooter>
+          </form>
+        </DialogContent>
+      </Dialog>
     </>
   );
 };
