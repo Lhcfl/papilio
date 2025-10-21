e14e6faeaf1b89431869f3a78c31aba304bd30b7
Lhcfl <lhcfl@outlook.com>
2025-10-21 19:03:18 +0800

    🚨 chore: make REUSE lint happy

--- src/hooks/use-uploading-hint.tsx
+++ src/hooks/use-uploading-hint.tsx
@@ -1,3 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: Linca and papilio-project
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
 import { UploadingToast } from '@/components/uploading-toast';
 import { useOnStartUpload } from '@/stores/upload-progress';
 import { toast } from 'sonner';
