4ff69cede8d178c46bd6b7ad2f6143317338d298
Lhcfl <lhcfl@outlook.com>
2025-11-20 00:29:40 +0800

    📄 chore: licence

--- src/components/app-default-error-component.tsx
+++ src/components/app-default-error-component.tsx
@@ -1,3 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: Linca and papilio-project
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
 import { MkError } from '@/components/mk-error';
 import { useQueryErrorResetBoundary } from '@tanstack/react-query';
 import { useRouter } from '@tanstack/react-router';
