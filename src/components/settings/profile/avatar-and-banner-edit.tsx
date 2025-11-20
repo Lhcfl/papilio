4271f82861c5afff98385cb07693f9184708b240
Lhcfl <lhcfl@outlook.com>
2025-11-21 03:58:58 +0800

    ✨ feat: add profile edit

--- src/components/settings/profile/avatar-and-banner-edit.tsx
+++ src/components/settings/profile/avatar-and-banner-edit.tsx
@@ -2,6 +2,7 @@ import { MkAvatar } from '@/components/mk-avatar';
 import { MkFileUploadMenu } from '@/components/mk-file-upload-menu';
 import { MkUserCardBanner } from '@/components/mk-user-card';
 import { Button } from '@/components/ui/button';
+import { useProfileEditAction } from '@/hooks/use-profile-edit-actions';
 import { useMe } from '@/stores/me';
 import { AwardIcon, ImageIcon } from 'lucide-react';
 import { useTranslation } from 'react-i18next';
@@ -11,9 +12,18 @@ export function AvatarAndBannerEdit() {
   const me = useMe();
   const { t } = useTranslation();
 
+  const { mutate } = useProfileEditAction();
+
   return (
     <div className="edit-avatar-and-banner bg-card relative overflow-hidden rounded-lg border">
-      <MkFileUploadMenu onUpload={() => toast.info('not implemented')} label={t('banner')}>
+      <MkFileUploadMenu
+        onUpload={async (promise) => {
+          const file = await promise[0];
+          mutate({ bannerId: file.id });
+        }}
+        label={t('banner')}
+        limit={1}
+      >
         <Button className="absolute top-3 left-3 z-10">
           <ImageIcon /> {t('_profile.changeBanner')}
         </Button>
@@ -25,7 +35,14 @@ export function AvatarAndBannerEdit() {
         avatarProps={{ className: 'size-30' }}
       />
       <div className="flex items-center justify-center gap-3 p-4 pt-18">
-        <MkFileUploadMenu onUpload={() => toast.info('not implemented')} label={t('avatar')}>
+        <MkFileUploadMenu
+          onUpload={async (promise) => {
+            const file = await promise[0];
+            mutate({ avatarId: file.id });
+          }}
+          label={t('avatar')}
+          limit={1}
+        >
           <Button>
             <ImageIcon /> {t('_profile.changeAvatar')}
           </Button>
--- src/routes/settings/profile.tsx
+++ src/routes/settings/profile.tsx
@@ -5,15 +5,20 @@
 
 import { AvatarAndBannerEdit } from '@/components/settings/profile/avatar-and-banner-edit';
 import { ProfileBaseInfoEdit } from '@/components/settings/profile/profile-base-info-edit';
+import { PageTitle } from '@/layouts/sidebar-layout';
 import { createFileRoute } from '@tanstack/react-router';
+import { useTranslation } from 'react-i18next';
 
 export const Route = createFileRoute('/settings/profile')({
   component: RouteComponent,
 });
 
 function RouteComponent() {
+  const { t } = useTranslation();
+
   return (
     <div className="px-1">
+      <PageTitle title={t('editProfile')} />
       <AvatarAndBannerEdit />
       <ProfileBaseInfoEdit />
     </div>
