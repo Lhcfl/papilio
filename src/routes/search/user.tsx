0ca46f9162b22acd778b5446cc440cdeff3147f6
Linca <lhcfl@outlook.com>
2025-12-02 19:49:34 +0800

    ✨ feat: add user search origin

--- src/routes/search/user.tsx
+++ src/routes/search/user.tsx
@@ -3,9 +3,10 @@
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
-import { MkInfiniteScroll, MkInfiniteScrollByData } from '@/components/infinite-loaders/mk-infinite-scroll';
-import { MkUserCard } from '@/components/mk-user-card';
+import { MkInfiniteScrollByData } from '@/components/infinite-loaders/mk-infinite-scroll';
 import { MkUserCardSmall } from '@/components/mk-user-card-small';
+import { Separator } from '@/components/ui/separator';
+import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { getAcctUserQueryOptions, getUserQueryOptions } from '@/hooks/user';
 import { misskeyApi } from '@/lib/inject-misskey-api';
 import { queryAtom } from '@/routes/search/-atoms';
@@ -13,6 +14,8 @@ import type { UserDetailed } from '@/types/user';
 import { useInfiniteQuery } from '@tanstack/react-query';
 import { createFileRoute } from '@tanstack/react-router';
 import { useAtomValue } from 'jotai';
+import { GlobeIcon, GlobeLockIcon, MapPinIcon } from 'lucide-react';
+import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
 export const Route = createFileRoute('/search/user')({
@@ -22,14 +25,16 @@ export const Route = createFileRoute('/search/user')({
 function RouteComponent() {
   const { t } = useTranslation();
   const query = useAtomValue(queryAtom);
+  const [origin, setOrigin] = useState<'local' | 'combined' | 'remote'>('combined');
 
   const q = useInfiniteQuery({
-    queryKey: ['search/users', query],
+    queryKey: ['search/users', query, origin],
     queryFn: ({ pageParam, client }) =>
       misskeyApi('users/search', {
         query,
         offset: pageParam,
         detail: true,
+        origin,
       }).then((ret) => {
         const users = ret as UserDetailed[];
         users.forEach((u) => {
@@ -47,12 +52,33 @@ function RouteComponent() {
   });
 
   return (
-    <div className="@container">
+    <Tabs
+      className="@container"
+      value={origin}
+      onValueChange={(v) => {
+        setOrigin(v as typeof origin);
+      }}
+    >
+      <TabsList className="mt-2">
+        <TabsTrigger value="combined">
+          <GlobeIcon /> {t('all')}
+        </TabsTrigger>
+        <TabsTrigger value="local">
+          <MapPinIcon /> {t('local')}
+        </TabsTrigger>
+        <TabsTrigger value="remote">
+          <GlobeLockIcon /> {t('remote')}
+        </TabsTrigger>
+      </TabsList>
       {query != '' && (
-        <MkInfiniteScrollByData infiniteQueryResult={q} containerClassName="grid @xl:grid-cols-2 gap-2">
-          {(u) => <MkUserCardSmall user={u} className="rounded-md border" />}
-        </MkInfiniteScrollByData>
+        <div className="search-result">
+          <Separator />
+          <div className="search-result-label text-muted-foreground p-2 text-sm">{t('searchResult')}</div>
+          <MkInfiniteScrollByData infiniteQueryResult={q} containerClassName="grid @xl:grid-cols-2 gap-2">
+            {(u) => <MkUserCardSmall user={u} className="rounded-md border" />}
+          </MkInfiniteScrollByData>
+        </div>
       )}
-    </div>
+    </Tabs>
   );
 }
